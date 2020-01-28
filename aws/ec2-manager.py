bbe01df8562ccaa4c2b78dd456a3568d6afa548a
Dheeraj Saini <dheeraj.saini@news.co.uk>
2020-01-28 13:15:58 +0000

    Add try catch to stop/start instances

--- aws/ec2-manager.py
+++ aws/ec2-manager.py
@@ -1,4 +1,5 @@
 import boto3
+import botocore
 import click
 
 session = boto3.Session(profile_name='dheeraj-sa-aws')
@@ -93,7 +94,11 @@ def start_instances(project):
 
     for i in instances:
         print("Starting {0}...".format(i.id))
-        i.start()
+        try:
+            i.start()
+        except botocore.exceptions.ClientError as e:
+            print("Could not start instance {0}. ".format(i.id) + str(e))
+            continue
     return
 
 @instances.command('stop')
@@ -104,7 +109,11 @@ def stop_instances(project):
 
     for i in instances:
         print("Stopping {0}...".format(i.id))
-        i.stop()
+        try:
+            i.stop()
+        except botocore.exceptions.ClientError as e:
+            print("Could not stop instance {0}. ".format(i.id) + str(e))
+            continue
     return
 
 @instances.command('list')
